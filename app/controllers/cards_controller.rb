48045deb40d1d3fa6021767b8bc2c763ce8664c0
Yihang Ho <hoyihang5@gmail.com>
2014-06-07 00:52:57 +0800

    Implement YAML import.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,11 +8,21 @@ class CardsController < ApplicationController
   end
 
   def create
-    @card = Card.create(card_params)
-    if @card.save
-      render 'show'
+    if params["card"]["yaml_file"]
+      cards = YAML.load(params["card"]["yaml_file"].read)
+      cards.each do |card|
+        card["word_type"] = card.delete("type")
+        sanitize!(card)
+        Card.create(card).save
+      end
+      redirect_to cards_path
     else
-      render 'new'
+      @card = Card.create(card_params)
+      if @card.save
+        render 'show'
+      else
+        render 'new'
+      end
     end
   end
 
@@ -29,5 +39,16 @@ class CardsController < ApplicationController
 
   def card_params
     params.require(:card).permit(:word, :word_type, :definition, :synonyms, :antonyms, :sentence)
+  end
+
+  def sanitize!(hash)
+    whitelist = [:word, :word_type, :definition, :synonyms, :antonyms, :sentence]
+    hash.each do |k, v|
+      if whitelist.include?(k.to_sym)
+        hash[k] = v.join(',') if v.is_a? Array
+      else
+        hash.delete(k)
+      end
+    end
   end
 end
