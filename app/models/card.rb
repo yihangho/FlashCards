b4cdaccd95414d953e6daf3531fb860734186f55
Yihang Ho <hoyihang5@gmail.com>
2014-06-07 10:28:58 +0800

    Implement weighted_random_order_score.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,12 @@
 class Card < ActiveRecord::Base
   validates :word, :presence => true
+
+  # weighted_random_order_score is the relative likelyhood of any instance being
+  # picked. Essentially, it is the product of hours since last revised * difference in rating between itself and the best performed card.
+  # The probability of this card being chosen is score/total score.
+  def weighted_random_order_score(now = Time.now, max_score = nil)
+    hours = ((now - updated_at.to_time)/3600).ceil
+    max_score = Card.order(:rating => :desc).take.rating if max_score.nil?
+    (max_score + 1 - rating) * hours
+  end
 end
