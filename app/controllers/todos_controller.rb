0fec1fb524c9d2a640d96396868cbe356c997eea
Yihang Ho <hoyihang5@gmail.com>
2014-06-09 13:23:27 +0800

    Able to create todo.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,24 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
   end
 
   def new
+    @todo = Todo.new
   end
 
   def create
+    @todo = Todo.create(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:word)
   end
 end
