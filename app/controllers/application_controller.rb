52e30f72b92fbb1d46d12908fbbbd9eb2c00e634
Yihang Ho <hoyihang5@gmail.com>
2014-06-09 13:56:28 +0800

    Implement render_404.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def render_404(message = "Not Found")
+    raise ActionController::RoutingError.new(message)
+  end
 end
