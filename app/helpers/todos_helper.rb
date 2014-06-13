c715ad2d3973c27b1d43e08ecbae9fe7b3163feb
Yihang Ho <hoyihang5@gmail.com>
2014-06-13 23:55:46 +0800

    Greatly improve the UX of deck edit page.

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,9 @@
 module TodosHelper
+  def select_options_arr(arr, selected_id)
+    output = arr.map do |item|
+      selected = selected_id.include?(item.id)
+      [item.word, item.id, {"selected" => selected}]
+    end
+    options_for_select output
+  end
 end
