8fe89c08b064656c2501af3dfdc52b5bd666e952
Yihang Ho <hoyihang5@gmail.com>
2014-06-14 15:26:25 +0800

    Generalize and move select_options_arr to ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def select_options_arr(arr, selected_val, value_sym, label_sym)
+    output = arr.map do |item|
+      selected = selected_val.include?(item.send(value_sym))
+      [item.send(label_sym), item.send(value_sym), {"selected" => selected}]
+    end
+    options_for_select output
+  end
 end
--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,9 +1,2 @@
 module TodosHelper
-  def select_options_arr(arr, selected_id)
-    output = arr.map do |item|
-      selected = selected_id.include?(item.id)
-      [item.word, item.id, {"selected" => selected}]
-    end
-    options_for_select output
-  end
 end
