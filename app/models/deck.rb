c1e8d2800a8830eb7e54a8d44cfb9e224033fc22
Yihang Ho <hoyihang5@gmail.com>
2014-06-24 23:39:13 +0800

    Implement Deck.find_or_create.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,20 @@ class Deck < ActiveRecord::Base
   validates :title, :presence => true
   validates :title, :uniqueness => true
   has_and_belongs_to_many :cards
+
+  def self.find_or_create(*args)
+    args.map do |title|
+      deck = Deck.find_by(:title => title)
+      if deck
+        deck
+      else
+        deck = Deck.create(:title => title)
+        if deck.save
+          deck
+        else
+          nil
+        end
+      end
+    end.delete_if { |deck| deck.nil? }
+  end
 end
